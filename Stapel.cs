9feeb009ee8711b8e7de033be39b51bd4b73ae2b
Karim Altir <mhd.altir@student.ehb.be>
2023-09-26 15:17:46 +0200

    making a class 'Stapel' with thier methods

---
+++ Stapel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneriekeCollecties
+{
+    internal class Stapel <T>
+    {
+        List <T> container;
+
+        internal Stapel()
+        {
+            container = new List <T>();
+        }
+
+        public void Opzetten(T i)
+        {
+            container.Add(i);
+        }
+
+        public T Afhallen()
+        {
+
+            T even = container[0];
+            container.RemoveAt(0);
+            return even;
+        }
+
+        public void LeegMaken()
+        {
+            container.Clear();
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+
+            foreach (T i in container)
+            {
+                s += i.ToString() + "\n";
+            }
+            return s;
+        }
+    }
+}
